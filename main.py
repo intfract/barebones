b30bf5a61daa303ed1cf92ef32a34f7aedeae5b2
Int Fract <110207136+intfract@users.noreply.github.com>
2022-12-28 22:40:53 +0000

    fixed `min` function

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 def max(a, b):
-    return a - ((a - b - abs(a - b)) / 2)
+    x = a - b
+    return a - ((x - abs(x)) / 2)
 
 print(max(0, 1)) # 1
 print(max(1, -1)) # 1
 print(max(-1, -2)) # -1
 
-def min(a, b): # (1 / 3) cases
-    return a + ((a - b + abs(a - b)) / 2)
+def min(a, b):
+    x = a - b
+    return a - ((x + abs(x)) / 2)
 
 print(min(0, 1)) # 0
 print(min(1, -1)) # -1
